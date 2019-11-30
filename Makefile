b24693e4d11515a4b1a4b9681f92b5144d38e1ef
timkutcher <tkutcher@dicorp.com>
2019-11-30 15:36:49 -0500

    add handles to icons

---
+++ Makefile
@@ -0,0 +1,12 @@
+.PHONY dev build deploy
+
+dev:
+	ng serve
+
+
+build:
+	npm run build
+
+
+deploy: build
+	npm run deploy
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface IProfileLink {
+  name: string;
+  iconClass: string;
+  listItemClass: string;
+  hexColor: string;
+  handle: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tkutcher-github-io';
+
+  profiles: IProfileLink[] = [
+    {
+      name: "linkedin",
+      iconClass: "icon-linkedin",
+      listItemClass: "tk-linkedin",
+      hexColor: "3b5998",
+      handle: "",
+      link: "https://www.linkedin.com/in/tim-kutcher-a7831416a/"
+    },
+    {
+      name: "facebook",
+      iconClass: "icon-facebook",
+      listItemClass: "tk-facebook",
+      hexColor: "3b5998",
+      handle: "",
+      link: "https://www.facebook.com/tim.kutcher.3"
+    },
+    {
+      name: "instagram",
+      iconClass: "icon-instagram",
+      listItemClass: "tk-instagram",
+      hexColor: "3b5998",
+      handle: "@tkutcher_",
+      link: "https://www.instagram.com/tkutcher_/"
+    },
+    {
+      name: "twitter",
+      iconClass: "icon-twitter",
+      listItemClass: "tk-twitter",
+      hexColor: "3b5998",
+      handle: "@tkutcher_",
+      link: "https://twitter.com/tkutcher_"
+    },
+    {
+      name: "youtube",
+      iconClass: "icon-youtube",
+      listItemClass: "tk-youtube",
+      hexColor: "3b5998",
+      handle: "",
+      link: "https://www.youtube.com/channel/UCHq9PYerV7OnYbJw5EHKQlA"
+    },
+    {
+      name: "github",
+      iconClass: "icon-github",
+      listItemClass: "tk-github",
+      hexColor: "3b5998",
+      handle: "@tkutcher",
+      link: "https://github.com/tkutcher"
+    },
+    {
+      name: "gitlab",
+      iconClass: "icon-gitlab",
+      listItemClass: "tk-gitlab",
+      hexColor: "3b5998",
+      handle: "@tkutcher",
+      link: "https://gitlab.com/tkutcher"
+    },
+    {
+      name: "stack-overflow",
+      iconClass: "icon-stack-overflow",
+      listItemClass: "tk-stack-overflow",
+      hexColor: "3b5998",
+      handle: "@tplusk",
+      link: "https://stackoverflow.com/users/9970629/tplusk"
+    },
+  ]
+
 }
